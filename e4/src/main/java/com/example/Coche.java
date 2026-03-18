1167175d41a0385f78b401417f9fa0845d35d99b
Aarondomin <aarondomin012@gmail.com>
2026-03-18 10:59:54 +0100

    Solución exercicio 4

--- e4/src/main/java/com/example/Coche.java
+++ e4/src/main/java/com/example/Coche.java
@@ -2,20 +2,38 @@ package com.example;
 
 public class Coche {
 
+    //*Marca del coche */
     private String marca;
+    /**Modelo del coche */
     private String modelo;
+    /**Velocidad del coche */
     private int velocidad;
 
+
+    /**
+     * Constructor para crear un nuevo coche
+     * @param marca marca del coche
+     * @param modelo modelo del coche
+     */
     public Coche(String marca, String modelo) {
         this.marca = marca;
         this.modelo = modelo;
         this.velocidad = 0;
     }
 
+    /**
+     * Incrementa la velocidad del coche
+     * @param incremento velocidad que se incrementa
+     */
+    
     public void acelerar(int incremento) {
-        velocidad += incremento;
+        velocidad += incremento; 
     }
 
+    /**
+     * Disminuye la velocidad del coche y no puede bajar de 0
+     * @param decremento velocidad que se disminuye
+     */
     public void frenar(int decremento) {
         velocidad -= decremento;
         if (velocidad < 0) {
@@ -23,6 +41,10 @@ public class Coche {
         }
     }
 
+    /**
+     * Obtine la velocidad
+     * @return devuelve la velocidad del coche
+     */
     public int getVelocidad() {
         return velocidad;
     }
--- e4/src/main/java/com/example/Libro.java
+++ e4/src/main/java/com/example/Libro.java
@@ -2,24 +2,45 @@ package com.example;
 
 public class Libro {
 
+    /** titulo del libro*/
     private String titulo;
+    /** autor del libro*/
     private String autor;
+    /** numero de paginas del libro*/
     private int paginas;
 
+    /**
+     * Constructor para crear un nuevo Libro
+     * @param titulo titulo del libro
+     * @param autor autor del libro
+     * @param paginas numero de paginas del libro
+     */
     public Libro(String titulo, String autor, int paginas) {
         this.titulo = titulo;
         this.autor = autor;
         this.paginas = paginas;
     }
 
+    /**
+     * Determina si un libro es largo o no
+     * @return devuelve true si el libro tiene mas de 300 paginas y false si tiene menos
+     */
     public boolean esLargo() {
         return paginas > 300;
     }
 
+    /**
+     * Obtiene el titulo del libro
+     * @return Devuelve el titulo del libro
+     */
     public String getTitulo() {
         return titulo;
     }
 
+    /**
+     * Obtine el autor del libro
+     * @return devuelve el autor del libro
+     */
     public String getAutor() {
         return autor;
     }
